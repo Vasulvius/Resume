27f4cfaf808924d24ed97e9d0f10558e3094f54e
rleurond <leurond.raphael@gmail.com>
2025-04-13 23:31:02 +0200

    feat: change to custom style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
-// // @ts-check
-// import { defineConfig } from 'astro/config';
-
-// // https://astro.build/config
-// export default defineConfig({});
-
+// @ts-check
 import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
-  output: 'static',
+    output: 'static',
 });
